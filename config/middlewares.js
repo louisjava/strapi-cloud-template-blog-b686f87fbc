9114c4904e1208d703d12cffb6d6e1deccf0e31d
cameron <cameron@xxx.com>
2024-04-10 20:19:23 +0800

    v202404102019

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,19 @@
-module.exports = {
-  settings: {
-    cors: {
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
       enabled: true,
-      origin: ['*']
-    },
+      headers: '*',
+      origin: ['http://localhost:1337', 'http://example2']
+    }
   },
-};
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
