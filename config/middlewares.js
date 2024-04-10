1a65f46f7118e5241387075865e09be1498e9de1
cameron <cameron@xxx.com>
2024-04-10 20:00:45 +0800

    v202404102000

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,12 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = {
+  load: {
+    before: ['timer', 'responseTime', 'logger', 'cors', 'responses', 'gzip'],
+    after: ['parser', 'router'],
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['*']
+    },
+  },
+};
